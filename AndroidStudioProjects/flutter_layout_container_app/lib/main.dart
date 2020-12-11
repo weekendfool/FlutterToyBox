989beb2f33b6cd0644fcc9df67bd0bfce151d795
Ohara <nori.tukareta@gmail.com>
2020-12-12 08:21:13 +0900

    テキストの追加

--- AndroidStudioProjects/flutter_layout_container_app/lib/main.dart
+++ AndroidStudioProjects/flutter_layout_container_app/lib/main.dart
@@ -12,10 +12,15 @@ class MyApp extends StatelessWidget {
     title: 'Flutter Material Design',
         home: Scaffold(
          body: Center(
+           //　表示されている四角形のウィジェット
           child: Container (
+            //　色の設定
             color: Colors.blue,
+            //　面積の設定
             width: 300.0,
-        height: 300.0,
+            height: 300.0,
+            //　textの設定
+            child: Text('冷やし中華始めました')
           ),
         ),
       ),
