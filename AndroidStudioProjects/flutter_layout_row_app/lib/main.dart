b77d57cc887178bee87dd99a31e3d9aef76a200d
Ohara <nori.tukareta@gmail.com>
2020-12-14 12:22:37 +0900

    入れ子構造の実装

--- AndroidStudioProjects/flutter_layout_row_app/lib/main.dart
+++ AndroidStudioProjects/flutter_layout_row_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp2());
+  runApp(MyApp3());
 }
 
 class MyApp extends StatelessWidget {
@@ -44,6 +44,44 @@ class MyApp2 extends StatelessWidget {
               Container(color: Colors.red, width: 100, height: 100),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp3 extends StatelessWidget {
+  @override
+  // 大枠のウィジェット作成
+  Widget build(BuildContext context) {
+    //　戻り値として大枠ウィジェットを返す？
+    return MaterialApp(
+      // 大枠のウィジェットのタイトル
+      title: 'Column & Row',
+      // 大枠ウィジェットの形式
+      home: Container(
+        // 子要素１の形式指定
+        child: Row(
+          // 子要素１の子要素を配列で作成
+          children: <Widget>[
+            // 子要素１の子要素１の形式指定
+          Column(
+            // 子要素１の子要素１の子要素を配列に格納
+          children: <Widget>[
+            // 子要素１の子要素１の子要素の詳細実装
+            Container(color: Colors.blue, width: 100, height: 100),
+            Container(color: Colors.red, width: 100, height: 100),
+          ],
+        ),
+            // 子要素１の子要素２の子要素を配列に格納
+        Column(
+          children: <Widget>[
+            // 子要素１の子要素２の子要素の詳細実装
+            Container(color: Colors.green, width: 100, height: 100),
+            Container(color: Colors.orange, width:100, height: 100),
+          ],
+        ),
+        ],
         ),
       ),
     );
