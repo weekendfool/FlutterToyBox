4b92df7e928a3ffc63ae9362ec80230745205d25
Ohara <nori.tukareta@gmail.com>
2020-12-14 11:36:19 +0900

    row実装

--- AndroidStudioProjects/flutter_layout_row_app/lib/main.dart
+++ AndroidStudioProjects/flutter_layout_row_app/lib/main.dart
@@ -9,17 +9,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Column & Row',
-      home: Containe(
-        child Center(
+      home: Container(
+        child: Center(
           child: Row(
           children:<Widget> [
-            
-          ],
+            Container(color: Colors.blue, width: 100, height: 100),
+            Container(color: Colors.red, width: 100, height: 100),
+            ],
+          ),
+        ),
       ),
-      ),
-      ),
-    ),
-
+    );
   }
-      )
 }
