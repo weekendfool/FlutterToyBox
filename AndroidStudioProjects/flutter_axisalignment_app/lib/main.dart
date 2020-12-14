5a4f7a2014db5f6c8fa79144985db24a5399291e
Ohara <nori.tukareta@gmail.com>
2020-12-14 16:32:31 +0900

    MainAxisAlignment.spaceBetween実装

--- AndroidStudioProjects/flutter_axisalignment_app/lib/main.dart
+++ AndroidStudioProjects/flutter_axisalignment_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp2());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         child: Container(
           color: Colors.white,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children:<Widget>[
               Container(color: Colors.blue, width: 50, height: 50),
               Container(color: Colors.red, width: 50, height: 50),
@@ -29,7 +29,23 @@ class MyApp extends StatelessWidget {
 
 class MyApp2 extends StatelessWidget {
   @override
-  Widget build(Buildcontext context) {
-    
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alignment2',
+        home: Center(
+          child: Container(
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:<Widget> [
+                Container(color: Colors.blue, width: 50, height: 50),
+                Container(color: Colors.red, width: 50, height: 50),
+                Container(color: Colors.green, width: 50, height: 50),
+                Container(color: Colors.orange, width: 50, height: 50),
+              ],
+            ),
+          ),
+        ),
+    );
   }
 }
