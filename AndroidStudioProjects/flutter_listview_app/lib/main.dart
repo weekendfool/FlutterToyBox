b7b15cd9f32e4c35f3a7877e32e737179c02a1d2
Ohara <nori.tukareta@gmail.com>
2020-12-18 16:18:34 +0900

    ListView改造

--- AndroidStudioProjects/flutter_listview_app/lib/main.dart
+++ AndroidStudioProjects/flutter_listview_app/lib/main.dart
@@ -1,23 +1,123 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp3());
 }
 
 class MyApp extends StatelessWidget {
   @override
+  //　大枠のビルド
   Widget build(BuildContext context) {
+    //　リストに表示するデータの配列の格納と宣言
     const data = [
       Text("item0"), Text("item1"), Text("item2"), Text("item3"), Text("item4")
     ];
-
+    //　画面表示する部分？
     return MaterialApp(
-      home:
-        Scaffold(
+      home: Scaffold(
+        //　listの実装部分
           body: ListView(
+            //　リストの持つデータとして、dataを指定
             children: data
           ),
         ),
+    );
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView'),
+        ),
+        body: ListView(
+          children: [
+            _menuItem("メニュー1", Icon(Icons.settings)),
+            _menuItem("メニュー2", Icon(Icons.map)),
+            _menuItem("メニュー3", Icon(Icons.room)),
+            _menuItem("メニュー4", Icon(Icons.local_shipping)),
+            _menuItem("メニュー5", Icon(Icons.airplanemode_active)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _menuItem(String title, Icon icon) {
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: new BoxDecoration(
+          border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
+        ),
+        child: Row(
+          children: <Widget> [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: icon,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0
+              ),
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        print("onTap called.");
+      },
+    );
+  }
+}
+
+class MyApp3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView'),
+        ),
+        body: ListView(
+          children: [
+            _menuItem("メニュー1", Icon(Icons.settings)),
+            _menuItem("メニュー2", Icon(Icons.map)),
+            _menuItem("メニュー3", Icon(Icons.room)),
+            _menuItem("メニュー4", Icon(Icons.local_shipping)),
+            _menuItem("メニュー5", Icon(Icons.airplanemode_active)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _menuItem(String title, Icon icon) {
+    return Container(
+      decoration: new BoxDecoration(
+        border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
+      ),
+      child: ListTile(
+        leading: icon,
+        title: Text(
+        title,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18.0
+          ),
+          ),
+        onTap: () {
+          print("onTap called.");
+        },
+        onLongPress: () {
+          print("onLongPress called.");
+        },
+      ),
     );
   }
 }
