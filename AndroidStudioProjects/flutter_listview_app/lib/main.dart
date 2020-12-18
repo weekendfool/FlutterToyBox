e2331d65a28f0bf9b5449b291edaacee9b31ae79
Ohara <nori.tukareta@gmail.com>
2020-12-18 12:43:15 +0900

    ListView実装

---
+++ AndroidStudioProjects/flutter_listview_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const data = [
+      Text("item0"), Text("item1"), Text("item2"), Text("item3"), Text("item4")
+    ];
+
+    return MaterialApp(
+      home:
+        Scaffold(
+          body: ListView(
+            children: data
+          ),
+        ),
+    );
+  }
+}
